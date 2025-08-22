24d887ec690ea2e040770cad0dfd4c1ef3323218
yasmincoos <yasmincostta.115@gmail.com>
2025-08-21 20:24:15 -0400

    first commit

---
+++ e1.c
@@ -0,0 +1,23 @@
+    #include <stdio.h>
+
+    int main ()
+    {
+    int inteiro1;
+    int inteiro2;
+
+    printf("digite o primeiro inteiro1\n");
+    scanf("%d, &inteiro1");
+    printf("digite o segundo inteiro2");
+    scanf("%d, &inteiro2");
+
+    soma = ("inteiro1 + inteiro2");
+
+    printf("A soma e %d\n", soma);
+
+    return 0;
+
+
+
+
+    }
+2
---
+++ e2.c
@@ -0,0 +1,18 @@
+    #include <stdio.h>
+
+    int main ()
+    {
+
+
+    float y, m, resultado = 0;
+
+    printf("entre no primeiro valor");
+    scanf("%f",&y);
+    printf("entre no segundo valor");
+    scanf("%f", &m);
+
+    resultado = y - m;
+
+    printf("a diferenca e :%F, resultado");
+    return 0;
+    }
